e5b354e876494a6fe919cb00a2cb29f8d19dde61
kaisjessa <kais.jessa@ucc.on.ca>
2018-05-24 10:59:43 -0400

    merged files

--- test/training-data.js
+++ test/training-data.js
@@ -1,3 +1,4 @@
+let trainedNet;
 const trainingData = [
 
   {
@@ -7,9 +8,27 @@ const trainingData = [
   {
     input: { r: 0.16, g: 0.09, b: 0.2 },
     output: { white: 1 }
-  },         
+  },
   {
     input: { r: 0.5, g: 0.5, b: 1.0 },
     output: { white: 1 }
   }
 ];
+
+
+
+function train(data) {
+  let net = new brain.NeuralNetwork();
+  net.train(trainingData);
+  trainedNet = net.toFunction();
+}
+
+function execute(input) {
+  console.log(input);
+  let results = trainedNet(input);
+  return results;
+}
+
+train(trainingData);
+let output = execute({ r: 0, g: 0.4, b: 0 });
+console.log(output)
